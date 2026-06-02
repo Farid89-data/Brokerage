42b8da873a74df8326080759cbe8e32c2afd6fdb
Farid Negahbani <frd.ngb.sn@gmail.com>
2026-06-02 12:13:18 +0200

    1st commit

---
+++ ETL/WebApi/old/GetSettlement111111/GetSettlement/GetSettlement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GetSettlement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
